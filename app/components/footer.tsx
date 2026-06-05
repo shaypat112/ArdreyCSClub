d764c9be1dc76c76d525d6980dc192e4f1001f43
Shivang Patel <shivangpatel2050@gmail.com>
2026-06-05 15:33:27 -0400

    check now

---
+++ app/components/footer.tsx
@@ -0,0 +1,25 @@
+export default function Footer() {
+  const contributors = [
+    {
+      id: 1,
+      name: "Shivang Patel",
+    },
+  ];
+
+  return (
+    <footer className="bg-black text-center text-sm text-zinc-400 py-4">
+      &copy; {new Date().getFullYear()} Ardrey Kell Computer Science Club. All
+      rights reserved.
+      <p>
+        This website was built by
+        <ul className="inline-flex gap-2 ml-1">
+          {contributors.map((contributor) => (
+            <li key={contributor.id} className="font-medium text-zinc-200">
+              {contributor.name}
+            </li>
+          ))}
+        </ul>
+      </p>
+    </footer>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Footer from "./components/footer";
 import { Navbar } from "./components/navbar";
 
 const geistSans = Geist({
@@ -28,6 +29,7 @@ export default function RootLayout({
       <body className="min-h-screen bg-black text-white">
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
